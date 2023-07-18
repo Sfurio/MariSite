eaacaf8f65f5e35cdad88d98aed9e15ddf26b4c8
Sfurio <77716014+Sfurio@users.noreply.github.com>
2023-07-17 20:02:52 -0400

    Update settings.py

--- mariiSite/mariiSite/settings.py
+++ mariiSite/mariiSite/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'mariiSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
+        'NAME': 'djangosite',
+        'USER': 'admin',
+        'PASSWORD': 'Gabs1022',
+        'HOST': 'localhost',
+        'PORT': ''
+        
     }
 }
 
